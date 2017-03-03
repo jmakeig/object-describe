1176dcf31cb59033ea2d287cc658d8ac0c7044fc
Justin Makeig <jmakeig@marklogic.com>
2017-03-03 10:57:10 -0800

    Instance typing and overrides

--- object-describe.js
+++ object-describe.js
@@ -1,9 +1,30 @@
 'use strict';
 
-const obj = { a: 'A', b: [1, 2, 3], c: null, d: Date.now(), e: undefined };
+function instanceType(obj) {
+  switch (typeof obj) {
+    case 'number':
+    case 'string':
+    case 'boolean':
+    case 'function':
+    case 'symbol':
+      return typeof obj;
+  }
+  if (null === obj) {
+    return 'null';
+  }
+  if (undefined === obj) {
+    return 'undefined';
+  }
+  if (obj.constructor && obj.constructor.name) {
+    return obj.constructor.name;
+  }
+  return Object.prototype.toString.call(obj) // [object Object]
+    .match(/^\[object (.+)\]$/)[1]; // Object
+}
 
 function describe(obj) {
   const props = [];
+  const inst = instanceType(obj);
   do {
     const propsAndSymbols = [].concat(
       Object.getOwnPropertyNames(obj),
@@ -11,31 +32,62 @@ function describe(obj) {
     );
     for (const prop of propsAndSymbols) {
       const p = { name: prop };
-      switch (typeof obj[prop]) {
+      const value = obj[prop];
+      switch (typeof value) {
         case 'string':
         case 'number':
         case 'date':
         case 'boolean':
         case 'function':
-          p.value = String(obj[prop]);
+        case 'symbol':
+          p.value = String(value);
           break;
         case 'object':
-        case 'symbol':
-          if (null === obj[prop] || undefined === obj[prop]) {
-            p.value = String(obj[prop]);
+          if (null === value || undefined === value) {
+            p.value = String(value);
           } else {
-            p.value = describe(obj[prop]);
+            p.value = describe(value);
           }
           break;
       }
 
       p.type = typeof obj[prop];
-      p.typeOf
+      p.from = instanceType(obj);
+      p.isEnumerable = obj.propertyIsEnumerable ? obj.propertyIsEnumerable(prop) : undefined;
+      
+      const overrides = props.filter(x => x.name === prop);
+      if (overrides.length > 0) {
+        p.overridden = true;
+        if(1 === overrides.length) {
+          overrides[0].overrideOf = p.from;
+        }
+      }
       props.push(p);
     }
   } while (obj = Object.getPrototypeOf(obj));
-  return props;
+  return { instance: inst, properties: props };
 }
 
-describe(obj);
+const obj = { a: 'A', b: [1, 2, 3], c: null, d: Date.now(), e: undefined };
+
+function Foo() {}
+Foo.prototype.fff = function() {};
+Foo.prototype[Symbol.toStringTag] = 'Foo';
+const foo = new Foo();
+
+const dict = Object.create(null);
+dict['a'] = 'A';
+dict.constructor === undefined; // true
+
+class Bar extends Foo {
+  bbb() {}
+  fff() {}
+}
+const bar = new Bar();
+bar.obj = obj;
+
+const baz = Object.create(Bar.prototype);
+
+describe(bar.obj.a);
+
 
