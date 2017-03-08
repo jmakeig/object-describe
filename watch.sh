b2252b4f84323527f10864ce6dce22c0dec93f97
Justin Makeig <jmakeig@marklogic.com>
2017-03-07 23:41:32 -0800

    Adds two-phased watcher
    1) any file change calls the main module (which updates rendered.html) 2) any change to rendered.html reloads the browser

--- watch.sh
+++ watch.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
-find . | grep -v '.git' | entr open -g rendered.html
+find . | grep -v '.git' | grep -v 'rendered.html' | entr curl -N --digest -u admin:admin http://localhost:8642/object-describe.sjs &
+find rendered.html | entr open -g rendered.html &
