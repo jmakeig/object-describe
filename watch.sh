a7eb05f83e005bb99172144a769fbfe760b358d2
Justin Makeig <jmakeig@marklogic.com>
2017-03-16 13:36:51 -0700

    Configures npm dependencies

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-find . | grep -v '.git' | grep -v 'rendered.html' | entr curl -N --digest -u admin:admin http://localhost:8642/object-describe.sjs &
+find . | grep -v '.git' | grep -v 'node_modules/' | grep -v 'rendered.html' | entr curl -N --digest -u admin:admin http://localhost:8642/object-describe.sjs &
 find rendered.html | entr open -g rendered.html &
