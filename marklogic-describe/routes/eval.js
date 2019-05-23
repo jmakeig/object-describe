45f1ce9e569352d30d9762f5d2c373cca0275da6
Justin Makeig <jmakeig@marklogic.com>
2019-05-23 09:50:35 -0700

    Pass-through eval

--- marklogic-describe/routes/eval.js
+++ marklogic-describe/routes/eval.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const marklogic = require('marklogic');
+
+const db = marklogic.createDatabaseClient({
+  host: 'localhost',
+  port: '8000',
+  database: 'Documents',
+  user: 'admin',
+  password: '********',
+  authType: 'DIGEST'
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  console.log(
+    db
+      .eval('const asdf={asdf:"asdf"}; asdf;')
+      .result(response => res.json(response[0].value))
+  );
+  //res.json(JSON.stringify({ asdf: 'asdf' }));
 });
 
 module.exports = router;
